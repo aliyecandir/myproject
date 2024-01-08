cdd08a2ce6dd8d03dd3e92606a4fa080bc4d8a5a
aliyecandir <candiraliyee@gmail.com>
2024-01-08 21:47:21 +0100

    My third commit :)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First Line");
         System.out.println("lovelyyy world");
+        System.out.println("You can do it :) ");
     }
 }
